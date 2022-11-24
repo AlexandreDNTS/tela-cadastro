ceb41be7607f757ff8152db14fe8a3245dc46194
Francimar Alexandre <92753716+AlexandreDNTS@users.noreply.github.com>
2022-11-24 13:58:51 -0300

    commit inicial

---
+++ main.py
@@ -0,0 +1,28 @@
+import PySimpleGUI as sg
+
+
+def tela_inicial():
+    layout = [
+        [sg.Text('\n\tTELA DE CADASTRO\t')],
+        [sg.Text('nome')],
+        [sg.Input(key='nome')],
+        [sg.Text('sobrenome')],
+        [sg.Input(key='sobrenome')],
+        [sg.Text('e-mail')],
+        [sg.Input(key='e-mail')],
+        [sg.Text('senha')],
+        [sg.Input(key='senha', password_char='*')],
+        [sg.Text('confirmar senha')],
+        [sg.Input(key='confirmar sennha', password_char='*')],
+        [sg.Button('cadastrar')],
+        [sg.Text('', key='msg')]
+    ]
+    return sg.Window('cadastro', layout=layout, finalize=True)
+
+
+telaInicial = tela_inicial()
+
+while True:
+    window, eventos, valores = sg.read_all_windows()
+    if eventos == sg.WIN_CLOSED:
+        break
